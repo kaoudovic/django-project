a42b9c9a67b288850b77d5cd254456c08c9bd472
kaoudovic <kaoudovic@gmail.com>
2020-05-09 13:25:07 +0200

    adding code to form

--- p2/app1/models.py
+++ p2/app1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class staff(models.Model):
     name=models.CharField(max_length=50)
-    email=models.EmailField()
+    email=models.EmailField(null=True)
     age=models.IntegerField()
     birthday=models.DateField()
 
--- p2/app1/views.py
+++ p2/app1/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, HttpResponse, redirect
 from . import models
+from .forms import staff_form
+from .models import staff
 
 
 def index(request):
     return HttpResponse('hello')
+
+
+def register(request):
+    form=staff_form(request.POST or None)
+    obj =staff()
+    if form.is_valid():
+        obj.name=form.cleaned_data['name']
+        obj.email=form.cleaned_data['email']
+        obj.age=form.cleaned_data['age']
+        obj.birthday=form.cleaned_data['birthday']
+        obj.save()
+        return redirect('/c3')
+    return render(request,'staff_reg.html',{'f':form})
