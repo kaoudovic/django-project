ac9cbde158313a64c9ecd90eca8e22d2b69c0c23
kaoudovic <kaoudovic@gmail.com>
2020-05-11 05:06:25 +0200

    add code

--- p2/app1/models.py
+++ p2/app1/models.py
@@ -7,6 +7,6 @@ class staff(models.Model):
     age=models.IntegerField()
     birthday=models.DateField()
     department=models.CharField(max_length=50,null=True)
-    
+
     def __str__(self):
         return self.name
